31559ba90588ba6c64d402e6a9a036fb8467b9fa
Javad Arjmandi <javadarjmandi@gmail.com>
2025-04-10 06:13:05 +0200

    adding config for ktlint

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Venues List"
+include(":venues")
+include(":venues:home")
+include(":venues:domain")
+include(":venues:data")
+include(":venues:presentation")
