42bcb112f1a254c010d401d512b5a02aa93a9754
Javad Arjmandi <javadarjmandi@gmail.com>
2025-04-10 11:19:06 +0200

    adding koin di

--- venues/data/src/main/java/de/arjmandi/venues/data/di/DataModule.kt
+++ venues/data/src/main/java/de/arjmandi/venues/data/di/DataModule.kt
@@ -3,10 +3,10 @@ package de.arjmandi.venues.data.di
 import de.arjmandi.venues.data.api.WoltApiService
 import de.arjmandi.venues.data.repository.VenueRepositoryImpl
 import de.arjmandi.venues.domain.repository.VenueRepository
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.serialization.kotlinx.json.*
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import org.koin.dsl.module
 
