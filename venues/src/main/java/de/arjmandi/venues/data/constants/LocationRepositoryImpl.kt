e000b2d6121c48e0d93e6c337ffcb18963ca5e4b
Javad Arjmandi <javad@arjmandi.de>
2025-05-31 00:16:26 +0200

    Refactor: Improve code readability and formatting across multiple files. ktlint.

--- venues/src/main/java/de/arjmandi/venues/data/constants/LocationRepositoryImpl.kt
+++ venues/src/main/java/de/arjmandi/venues/data/constants/LocationRepositoryImpl.kt
@@ -5,33 +5,32 @@ import de.arjmandi.venues.domain.repository.LocationRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.launch
 
 class LocationRepositoryImpl(
-	private val coroutineScope: CoroutineScope
+	coroutineScope: CoroutineScope,
 ) : LocationRepository {
-
-	private val _locationFlow: SharedFlow<Location> = callbackFlow {
-		var index = 0
-		val job = launch {
-			while (true) {
-				trySend(Location.coordinates[index])
-				delay(10_000)
-				index = (index + 1) % Location.coordinates.size
-			}
-		}
-		awaitClose { job.cancel() }
-	}.shareIn(
-		scope = coroutineScope,
-		started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 0),
-		replay = 1
-	)
+	private val _locationFlow: SharedFlow<Location> =
+		callbackFlow {
+			var index = 0
+			val job =
+				launch {
+					while (true) {
+						trySend(Location.coordinates[index])
+						delay(10_000)
+						index = (index + 1) % Location.coordinates.size
+					}
+				}
+			awaitClose { job.cancel() }
+		}.shareIn(
+			scope = coroutineScope,
+			started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 0),
+			replay = 1,
+		)
 
 	override val locationFlow: SharedFlow<Location> = _locationFlow
 }
